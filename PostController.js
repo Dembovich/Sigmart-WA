2affd7e28ccefee65a3e826931c66331a20e2f0a
Dembovich <demboviches@gmail.com>
2023-09-04 01:03:00 +0300

    init

---
+++ PostController.js
@@ -0,0 +1,66 @@
+﻿
+let arrName = [];
+class PostController {
+
+
+    async create(req, res) {
+        try {
+
+            console.log(req.body);
+            arrName.push(req.body.name)
+            res.status(200).json({ state: 'Сервер работает!', message: 'Добавлен name - ' + req.body.name })
+
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async getAll(req, res) {
+        try {
+            console.log(req);
+            res.status(200).json({ state: 'Сервер работает!', array: arrName })
+
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async getOne(req, res) {
+        try {
+
+            console.log(req.body);
+            res.status(200).json('Сервер работает!')
+
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async update(req, res) {
+        try {
+
+            console.log(req.body);
+            res.status(200).json('Сервер работает!')
+
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async delete(req, res) {
+        try {
+
+            console.log(req.body);
+            res.status(200).json('Сервер работает!')
+
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+
+
+}
+
+
+export default new PostController();
---
+++ index.js
@@ -0,0 +1,23 @@
+﻿import express from 'express'
+import router from "./router.js"
+
+const PORT = 3000;
+
+const app = express()
+
+app.use(express.json())
+app.use('/api', router)
+
+async function startApp() {
+    try {
+
+        // await Подключение к БД
+        app.listen(PORT, () => console.log('Server started on port - ' + PORT))
+
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
